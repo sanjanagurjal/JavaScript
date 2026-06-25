254db0d73f5cea220af9985384e4cfd60a7ee27b
Sanjana Gurjal <sanjana.gurjal@gmail.com>
2026-06-25 18:53:58 +0530

    Day 1 - JavaScript Basics

---
+++ script.js
@@ -0,0 +1,42 @@
+//Variables storing information
+let myName = "Ram";
+let myAge = "21";
+let isStudent = true;
+
+//Console log - prints to browser console
+
+console.log("Hello World!");
+console.log("My name is " + myName);
+console.log("My age is "+ myAge);
+console.log("Am I a student? "+ isStudent);
+
+//Data Types
+let text = "Hello";
+let number = 24;
+let decimal = 3.14;
+let isTrue = true;
+let nothing = null;
+let notDefined;
+
+//typeof - check what type something is
+console.log(typeof text);
+console.log(typeof number);
+console.log(typeof isTrue);
+
+//string operations
+let firstName ="Ram";
+let lastName="Verma";
+let fullName= firstName+" "+ lastName;
+console.log("Full name: " + fullName);
+console.log("Name length: " + fullName.length);
+console.log("Uppercase:" + fullName.toUpperCase());
+console.log("Lowercase:" + fullName.toLowerCase());
+
+//Number Operations
+let a = 10;
+let b = 3;
+console.log("Add:"+ (a+b));
+console.log("Subtract:"+(a - b));
+console.log("Multiply:"+(a * b));
+console.log("Divide:"+(a / b));
+console.log("Remainder:"+(a % b));
